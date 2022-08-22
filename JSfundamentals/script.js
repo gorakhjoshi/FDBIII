aa1bdc1aa234eda1054a07e3a3bf047dcd6b0274
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-08-22 07:19:41 +0545

    data types

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,6 +1,39 @@
-alert('Hello');
+const interestRate = "16%";
+console.log(interestRate);
 
-// Terminal (Node.js)
-// Browser
+// number
+// String
 
-console.log("Hello from JavaScript");
+// Array
+// Object
+// Function
+
+const myCountry = "Nepal";
+console.log(myCountry);
+
+const age = 45;
+
+console.log(typeof age);
+
+// const evenNumbers = [2, 4, 6, 8, ];
+const evenNumbers = new Array(2, 4, 6, 8, "Hello", [1, 2]);
+console.log(evenNumbers[3]);
+
+const bootcamp = {
+  duration: "3 months",
+  course: "React",
+};
+console.log(bootcamp.duration);
+
+const arrayBootcamp = ["3 months", "React"];
+console.log(arrayBootcamp[1]);
+
+function addTwoNumber(first, second) {
+  return first + second;
+}
+
+const result = addTwoNumber(10, 20);
+console.log(result);
+
+const secondResult = addTwoNumber(13240, 23240);
+console.log(secondResult);
