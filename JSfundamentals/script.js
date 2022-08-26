5469fefd5dbab800bf024dc2738659088f5b1262
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-08-26 07:36:46 +0545

    strict mode and IIFE

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -25,3 +25,34 @@ add();
 if (true) {
   console.log(this);
 }
+
+const Private = "Hospital One";
+console.log(Private);
+
+const country = "Nepal";
+
+const Country = "India";
+
+let lockdownSituations = false;
+const canDrive = true;
+
+if (canDrive) {
+  // lockdownSituation = true;
+  lockdownSituations = true;
+}
+
+if (lockdownSituations) {
+  console.log("Hey! You can not drive furing lockdown.");
+}
+
+const arr = [1, 2, 3, 4];
+for (const num of arr) {
+  console.log(num);
+}
+
+// const myName = "Gorakh";
+
+(function () {
+  console.log("Hello");
+})();
+// IIFE
