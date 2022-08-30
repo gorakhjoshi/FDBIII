974c74834d0a1f509a6c65b5fc1ec7300e9c5780
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-08-30 07:47:08 +0545

    Promise in Javascript

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+"use strict";
 
+const url = "https://api.github.com/users/gorakhjoshi";
+
+// const response = fetch(url);
+
+// response.then(function () {
+//   console.log("Your promise is fulfilled");
+// });
+
+// response.catch(function () {
+//   console.log("Error");
+// });
+
+console.log("Start");
+const response = fetch(url)
+  .then((res) => {
+    console.log("First Then");
+    return res.json();
+  })
+  .then((data) => {
+    console.log("Second Then");
+    console.log(data);
+  })
+  .catch((e) => console.log(e));
+
+console.log(response);
+
+console.log("End");
+
+for (let i = 0; i < 100000; i++) {
+  console.log("Running");
+}
