7fadd0692cc8797980b9aba2b0bf33e405860fe3
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-02 07:50:08 +0545

    typing array and function

--- typescript/app.ts
+++ typescript/app.ts
@@ -7,5 +7,42 @@ movieTitle = "3 Idiots";
 
 //Any
 let anything: any = 9;
-anything = 'Nepal';
-anything = true
+anything = "Nepal";
+anything = true;
+
+let ages: (number | string)[][];
+ages = [[1, 2], ["hello"]];
+
+const myNumbers = [1, 2, 3, 4];
+// myNumbers.push(5);
+// myNumbers = [1, 2, 3, 4];
+// console.log([1, 2] === [1, 2]);
+
+function display(a: number, b: number) {
+  console.log(a);
+  console.log(b);
+}
+
+// display(2, 'hello');
+display(2, 10);
+
+function add(a: string, b: string): string {
+  return a + b;
+}
+
+const result = add("10", "20");
+console.log(result);
+
+function print(value: number): void {
+  console.log(value);
+}
+
+print(10);
+
+// let error = new Error("hello");
+
+// console.log(error);
+
+// let arr;
+
+// console.log(arr)
