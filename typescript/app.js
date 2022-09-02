284850bbe796f76f238ad124f713b976f76a0651
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-02 07:05:24 +0545

    type annotation and type inference

--- typescript/app.js
+++ typescript/app.js
@@ -1,1 +1,9 @@
-console.log('Running');
+// let movieTitle = "Name";
+// Type inference
+var movieTitle = "Name";
+// Type annotation
+movieTitle = "3 Idiots";
+//Any
+var anything = 9;
+anything = 'Nepal';
+anything = true;
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,11 @@
-console.log('Running')
+// let movieTitle = "Name";
+// Type inference
+
+let movieTitle: string = "Name";
+// Type annotation
+movieTitle = "3 Idiots";
+
+//Any
+let anything: any = 9;
+anything = 'Nepal';
+anything = true
