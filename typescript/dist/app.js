71443c6d8bd0581081bbd2c8fc8971c7ca414817
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-05 06:55:47 +0545

    primitive types

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('app');
+// 1. Primitive Types
+const isAvailable = true;
+// let isAvailableLet = 20;
+let age = 30;
+let hello = "world";
+let notDefined = undefined;
+let notPresent = null;
+notPresent = 30;
+// notPresent = '30';
+// notPresent = ['30'];
+console.log(Symbol("unique") === Symbol("unique"));
+// let bigNUmber = BigInt("23123123131312312312312331312");
+// console.log(bigNUmber)
+let bigNUmber = 20n;
--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,1 +1,18 @@
-console.log('app')
+// 1. Primitive Types
+const isAvailable: boolean = true;
+// let isAvailableLet = 20;
+let age = 30;
+let hello = "world";
+
+let notDefined = undefined;
+let notPresent: number | null = null;
+notPresent = 30;
+// notPresent = '30';
+// notPresent = ['30'];
+
+console.log(Symbol("unique") === Symbol("unique"));
+
+// let bigNUmber = BigInt("23123123131312312312312331312");
+// console.log(bigNUmber)
+
+let bigNUmber = 20n;
