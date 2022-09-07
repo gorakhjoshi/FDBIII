0daae26b1d6690c7d48b9926f9c523e9a7f7c21d
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-07 07:39:20 +0545

    typescript assertion and promise

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,19 +1,59 @@
 "use strict";
-// typescript generic
-class Queue {
-    data = [];
-    push(item) {
-        this.data.push(item);
-    }
-    pop() {
-        return this.data.shift();
-    }
+// // typescript generic
+// class Queue<T> {
+//   data: Array<T> = [];
+//   push(item: T) {
+//     this.data.push(item);
+//   }
+//   pop(): T | undefined {
+//     return this.data.shift();
+//   }
+// }
+// const firstObj = new Queue<number>();
+// firstObj.push(30);
+// // firstObj.push('40');
+// console.log(firstObj);
+// const secondObj: Queue<string> = new Queue();
+// secondObj.push("30");
+// secondObj.push("hello");
+// console.log(secondObj);
+// const arr = new Array<number>(1, 2, 3, 4);
+function outputArray(valueOne, valueTwo) {
+    return [valueOne, valueTwo];
 }
-const firstObj = new Queue();
-firstObj.push(30);
-firstObj.push(40);
-console.log(firstObj);
-const secondObj = new Queue();
-secondObj.push("30");
-secondObj.push("hello");
-console.log(secondObj);
+// function outputArray(valueOne, valueTwo) {
+//   return [valueOne, valueTwo];
+// }
+console.log(outputArray(1, "hello"));
+// outputArray.pop();
+// function getArray(items) {
+//   return new Array().concat(items);
+// }
+// const outputNumber = getArray([1, 2, 3, 4]);
+// console.log(outputNumber);
+// const outputString = getArray(["hello", "World"]);
+// console.log(outputString);
+// outputNumber.push("Nepal");
+// console.log(outputNumber);
+function getArray(items) {
+    return new Array().concat(items);
+}
+const outputNumber = getArray([1, 2, 3, 4]);
+console.log(outputNumber);
+const outputString = getArray(["hello", "World"]);
+console.log(outputString);
+// outputNumber.push(10);
+// console.log(outputNumber);
+let value;
+// value = Number('10.67');
+value = +"10.67";
+console.log(value.toFixed(1));
+// console.log((<number>value).toFixed(1)); do not use
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+async function print() {
+    await delay(1000);
+    console.log("1s");
+    await delay(1000);
+    console.log("2s");
+}
+print();
--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,22 +1,83 @@
-// typescript generic
-class Queue<T> {
-  data: Array<T> = [];
+// // typescript generic
+// class Queue<T> {
+//   data: Array<T> = [];
 
-  push(item: T) {
-    this.data.push(item);
-  }
+//   push(item: T) {
+//     this.data.push(item);
+//   }
 
-  pop(): T | undefined {
-    return this.data.shift();
-  }
+//   pop(): T | undefined {
+//     return this.data.shift();
+//   }
+// }
+
+// const firstObj = new Queue<number>();
+// firstObj.push(30);
+// // firstObj.push('40');
+// console.log(firstObj);
+
+// const secondObj: Queue<string> = new Queue();
+// secondObj.push("30");
+// secondObj.push("hello");
+// console.log(secondObj);
+
+// const arr = new Array<number>(1, 2, 3, 4);
+
+function outputArray<T, U>(valueOne: T, valueTwo: U): [T, U] {
+  return [valueOne, valueTwo];
 }
 
-const firstObj: Queue<number> = new Queue();
-firstObj.push(30);
-firstObj.push(40);
-console.log(firstObj);
+// function outputArray(valueOne, valueTwo) {
+//   return [valueOne, valueTwo];
+// }
 
-const secondObj: Queue<string> = new Queue();
-secondObj.push("30");
-secondObj.push("hello");
-console.log(secondObj);
+console.log(outputArray(1, "hello"));
+
+// outputArray.pop();
+
+// function getArray(items) {
+//   return new Array().concat(items);
+// }
+
+// const outputNumber = getArray([1, 2, 3, 4]);
+// console.log(outputNumber);
+
+// const outputString = getArray(["hello", "World"]);
+// console.log(outputString);
+
+// outputNumber.push("Nepal");
+// console.log(outputNumber);
+
+function getArray<T>(items: T): T[] {
+  return new Array<T>().concat(items);
+}
+
+const outputNumber = getArray<number[]>([1, 2, 3, 4]);
+console.log(outputNumber);
+
+const outputString = getArray<string[]>(["hello", "World"]);
+console.log(outputString);
+
+// outputNumber.push(10);
+// console.log(outputNumber);
+
+let value: unknown;
+// value = Number('10.67');
+value = +"10.67";
+console.log((value as number).toFixed(1));
+
+// console.log((<number>value).toFixed(1)); do not use
+
+const delay = (ms: number): Promise<unknown> =>
+  new Promise((res) => setTimeout(res, ms));
+
+async function print() {
+  await delay(1000);
+  console.log("1s");
+
+  await delay(1000);
+  console.log("2s");
+
+}
+
+print();
