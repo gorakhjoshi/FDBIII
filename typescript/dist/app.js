4c0e159bbc5a2767139a405a8ba7550290eb95c2
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-07 06:52:12 +0545

    typescript generic example 1

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,30 +1,19 @@
 "use strict";
-let exampleAny;
-exampleAny = 30;
-console.log(typeof exampleAny);
-exampleAny = "hello";
-exampleAny = [1, 2, 3];
-console.log(exampleAny);
-let exampleUnknown;
-exampleUnknown = 30;
-console.log(typeof exampleUnknown);
-exampleUnknown = "30";
-console.log(typeof exampleUnknown);
-exampleUnknown = ["30"];
-console.log(typeof exampleUnknown);
-let numbers;
-numbers = 40;
-numbers = 50.1678;
-// numbers = "15.879";
-// console.log(numbers.toFixed(1));
-// numbers.i.wish.to.us.this();
-// any -> unknown
-// if (typeof numbers === "number") {
-//   console.log("I am a number");
-//   console.log(numbers.toFixed(1));
-// }
-// if (typeof numbers === "string") {
-//   console.log("I am a string");
-// }
-typeof numbers === "number" && console.log(numbers.toFixed(1));
-console.log(typeof numbers === "number");
+// typescript generic
+class Queue {
+    data = [];
+    push(item) {
+        this.data.push(item);
+    }
+    pop() {
+        return this.data.shift();
+    }
+}
+const firstObj = new Queue();
+firstObj.push(30);
+firstObj.push(40);
+console.log(firstObj);
+const secondObj = new Queue();
+secondObj.push("30");
+secondObj.push("hello");
+console.log(secondObj);
--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,40 +1,22 @@
-let exampleAny;
+// typescript generic
+class Queue<T> {
+  data: Array<T> = [];
 
-exampleAny = 30;
-console.log(typeof exampleAny);
-exampleAny = "hello";
-exampleAny = [1, 2, 3];
+  push(item: T) {
+    this.data.push(item);
+  }
 
-console.log(exampleAny);
+  pop(): T | undefined {
+    return this.data.shift();
+  }
+}
 
-let exampleUnknown: unknown;
-exampleUnknown = 30;
-console.log(typeof exampleUnknown);
-exampleUnknown = "30";
-console.log(typeof exampleUnknown);
-exampleUnknown = ["30"];
-console.log(typeof exampleUnknown);
+const firstObj: Queue<number> = new Queue();
+firstObj.push(30);
+firstObj.push(40);
+console.log(firstObj);
 
-let numbers: unknown;
-numbers = 40;
-numbers = 50.1678;
-// numbers = "15.879";
-
-// console.log(numbers.toFixed(1));
-
-// numbers.i.wish.to.us.this();
-
-// any -> unknown
-
-// if (typeof numbers === "number") {
-//   console.log("I am a number");
-//   console.log(numbers.toFixed(1));
-// }
-
-// if (typeof numbers === "string") {
-//   console.log("I am a string");
-// }
-
-typeof numbers === "number" && console.log(numbers.toFixed(1));
-
-console.log(typeof numbers === "number")
+const secondObj: Queue<string> = new Queue();
+secondObj.push("30");
+secondObj.push("hello");
+console.log(secondObj);
