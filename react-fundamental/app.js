b5a455c85af1471572b5967cf84db7a81dcffc57
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-08 07:25:33 +0545

    Raw react apis

--- react-fundamental/app.js
+++ react-fundamental/app.js
@@ -1,18 +1,31 @@
 "use strict";
-// Creating sourceElement(div) element
-const sourceElement = document.createElement("div");
-sourceElement.id = "root";
+// // Creating sourceElement(div) element
+// const sourceElement = document.createElement("div");
+// sourceElement.id = "root";
 
-// Targeting destination
-const destination = document.body;
+// // Targeting destination
+// const destination = document.body;
 
-// Sending sourceElement(div) to destination
-destination.append(sourceElement);
+// // Sending sourceElement(div) to destination
+// destination.append(sourceElement);
 
-// Creating another div
-const divElement = document.createElement("div");
-divElement.textContent = "Hello World";
-divElement.className = "container";
+// // Creating another div
+// const divElement = document.createElement("div");
+// divElement.textContent = "Hello World";
+// divElement.className = "container";
 
-// Sending divElement to sourceElement
-sourceElement.append(divElement);
+// // Sending divElement to sourceElement
+// sourceElement.append(divElement);
+
+// console.log(React);
+
+const rootElement = document.getElementById("root");
+const element = React.createElement("div", {
+  className: "container",
+  children: [
+    React.createElement("span", null, "Hello"),
+    React.createElement("span", null, "Hello"),
+  ],
+});
+
+ReactDOM.render(element, rootElement);
