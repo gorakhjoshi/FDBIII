b2040ea1f40357fa3908df61c26326b778bcf515
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-08 06:49:29 +0545

    basic javascript rendered hello world

--- react-fundamental/app.js
+++ react-fundamental/app.js
@@ -1,1 +1,18 @@
-console.log(React);
+"use strict";
+// Creating sourceElement(div) element
+const sourceElement = document.createElement("div");
+sourceElement.id = "root";
+
+// Targeting destination
+const destination = document.body;
+
+// Sending sourceElement(div) to destination
+destination.append(sourceElement);
+
+// Creating another div
+const divElement = document.createElement("div");
+divElement.textContent = "Hello World";
+divElement.className = "container";
+
+// Sending divElement to sourceElement
+sourceElement.append(divElement);
