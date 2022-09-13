ecf6d10c6affe20c0b9cf222a1b201e11df78f23
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-13 07:32:02 +0545

    useState hook in react

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,10 +1,38 @@
-import Lists from "./Lists";
+import { useState } from "react";
 
 function App() {
+  const [person, setPerson] = useState({ firstName: "Laxman", age: 30 });
+  const [count, setCount] = useState(0);
+
+  console.log("Running App!");
+  console.log(person);
+
+  function ageHandler() {
+    // function (prev) {
+    //   return { ...prev, age: 40 };
+    // }
+    console.log("Setting state of Person");
+    setPerson((prev) => ({ ...prev, age: prev.age + 1 }));
+    console.log("Now re-rendering if state changed!");
+  }
+
+  function countHandler() {
+    console.log("Setting count");
+    setCount(() => count + 1);
+    console.log("Now re-rendering if state changed!");
+  }
+
   return (
     <div>
-      <div>App</div>
-      <Lists />
+      {person.firstName}
+      <br />
+      {person.age}
+      <br />
+      <button onClick={ageHandler}>Increase Age</button>
+      <br />
+      {count}
+      <br />
+      <button onClick={countHandler}>Increase Count</button>
     </div>
   );
 }
--- create-react-app/src/List.js
+++
@@ -1,5 +0,0 @@
-function List() {
-  return <div>Single List</div>;
-}
-
-export default List;
