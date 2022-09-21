535a229c2c2794269fcb37332c980445945c0cfb
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-21 07:16:11 +0545

    initial exercise

--- intermediate-react/src/App.js
+++ intermediate-react/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// useReducer: simple Counter
+
+import * as React from "react";
+
+function countReducer(state, newState) {
+  console.log("Running countReducer Function!");
+  console.log("value of state", state);
+  console.log("value of newState", newState);
+  return newState;
+}
+
+function Counter({ initialCount = 0 }) {
+  const [count, setCount] = React.useReducer(countReducer, initialCount);
+  console.log("Counter is Running");
+  console.log("Current Value of count", count);
+
+  const increment = () => setCount(count + 1);
+
+  return <button onClick={increment}>{count}</button>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log("App is running");
+  return <Counter />;
 }
 
 export default App;
