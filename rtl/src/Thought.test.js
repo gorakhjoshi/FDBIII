d9d74d0d7f8f39b6e0a53b2818a0d2da121cdac6
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-14 06:24:50 +0545

    querying with RTL

--- rtl/src/Thought.test.js
+++ rtl/src/Thought.test.js
@@ -1,15 +1,27 @@
 import { Thought } from "./Thought.js";
 import { render, screen } from "@testing-library/react";
+import App from "./App.js";
 
-test("Should test Thought component", () => {
-  render(
-    <Thought
-      thought={{
-        text: "Testing thoughts",
-      }}
-      removeThought={() => {}}
-    />
-  );
-  // eslint-disable-next-line testing-library/no-debugging-utils
-  screen.debug();
+describe("Testing Thought Component", () => {
+  test("Should test Thought component", () => {
+    render(
+      <Thought
+        thought={{
+          text: "Testing thoughts",
+        }}
+        removeThought={() => {}}
+      />
+    );
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.debug();
+  });
+});
+
+describe("Testing App component", () => {
+  test("Should render Passing Thoughts text", () => {
+    render(<App />);
+
+    const header = screen.getByText("Passing Thoughts");
+    expect(header).toHaveTextContent("Passing Thoughts");
+  });
 });
