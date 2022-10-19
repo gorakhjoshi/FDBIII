e5b558f6615d4e4a5f06e7f8abcfa8b9f170cb0c
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-19 07:31:26 +0545

    get request with axios

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,4 +1,33 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+// Using fetch
+// async function getCatsData() {
+//   try {
+//     const response = await fetch("https://api.thecatapi.com/v1/breeds");
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// Using axios
+async function getCatsData() {
+  try {
+    const response = await axios("https://api.thecatapi.com/v1/breeds");
+    console.log(response);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
 function App() {
+  useEffect(() => {
+    getCatsData();
+  }, []);
   return <div>App</div>;
 }
 
