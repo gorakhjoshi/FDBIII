0a9dba6dd5e12cf753614363752e619fe834c5ab
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-19 06:17:00 +0545

    initial setup for react performance

---
+++ react-performance/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const plusFive = (num) => {
+  console.log("i was called!");
+  return num + 5;
+};
+
+export default function App() {
+  const [num, setNum] = useState(0);
+  const [light, setLight] = useState(true);
+  const numPlusFive = plusFive(num);
+  return (
+    <div className={light ? "light" : "dark"}>
+      <div>
+        <h1>Without useMemo</h1>
+        <h2>
+          Current number: {num}, Plus five: {numPlusFive}
+        </h2>
+        <div className="button-container">
+          <button
+            onClick={() => {
+              setNum(num + 1);
+            }}
+          >
+            Update Number{" "}
+          </button>
+          <button
+            onClick={() => {
+              setLight(!light);
+            }}
+          >
+            {" "}
+            Toggle the light{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
