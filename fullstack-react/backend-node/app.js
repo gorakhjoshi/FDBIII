a18fe55020c3b2c99eec3fc0ac6cbf2c721de0a7
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-20 07:26:59 +0545

    created /api/v1/tasks api

--- fullstack-react/backend-node/app.js
+++ fullstack-react/backend-node/app.js
@@ -1,13 +1,24 @@
-// ES module
-// import express from "express";
-
-// commonjs modules
 const express = require("express");
+// const router = express.Router();
 
 const app = express();
 
-app.get("/v1/breeds", (req, res) => {
-  res.send("Hello");
+// router.route("/").get((req, res) => {
+//   res.send("Hello From Main Route");
+// });
+
+// app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Hello From /hello route");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello From Main Route");
+});
+
+app.use("/api/v1/tasks", (req, res) => {
+  res.json({ response: "Welcome to task api" });
 });
 
 const port = 4000;
