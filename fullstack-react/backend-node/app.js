a25cb5440df095b4efb0a5ce8e5c88d972a619f7
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-24 07:13:29 +0545

    connecting with dotenv and .env

--- fullstack-react/backend-node/app.js
+++ fullstack-react/backend-node/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const connectionString = `mongodb+srv://reacttask:${process.env.DBPassword}@cluster0.s7zzzbn.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to Database..."))
+  .catch((error) => console.log(error));
 
 const app = express();
 
